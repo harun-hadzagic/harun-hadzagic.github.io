51017b6f99bddfdcd0feeef50385c931ebacb618
harun-hadzagic <harun.hadzagic@gmail.com>
2022-08-17 13:39:13 +0200

    Initial commit

---
+++ js/countdown.js
@@ -0,0 +1,28 @@
+const daysElement = document.getElementById(`days`);
+const hoursElement = document.getElementById(`hours`);
+const secondsElement = document.getElementById(`seconds`);
+const minutesElement = document.getElementById(`mins`);
+
+const newYear = `1 Jan 2023`
+
+function countdown (){
+    const newYearsDate = new Date (newYear);
+    const currentDate = new Date();
+
+    const milliseconds = newYearsDate-currentDate;
+    const totalseconds = Math.floor(milliseconds / 1000);
+    const seconds = Math.floor(milliseconds / 1000 %60);
+    const minutes = Math.floor(totalseconds/60) %60;
+    const hours = (Math.floor(totalseconds/3600) %24)-8;
+    const days = Math.floor((totalseconds / 3600)/24);
+
+    daysElement.innerHTML = days;
+    hoursElement.innerHTML = hours;
+    secondsElement.innerHTML = seconds;
+    minutesElement.innerHTML = minutes;
+}
+
+
+countdown();
+
+setInterval(countdown, 1000);
